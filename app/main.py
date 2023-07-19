f03c8f7975c44d8825b3825ca27b5163c2fad17b
ta-kiat <33117085+ta-kiat@users.noreply.github.com>
2023-07-20 04:07:25 +0700

    handle device disconnect

--- app/main.py
+++ app/main.py
@@ -2,8 +2,11 @@ from fastapi import FastAPI
 import os
 import asyncio
 import prometheus_client
+import traceback
+import re
 from prometheus_client import make_asgi_app, Gauge
 from miio import DeviceFactory
+from loguru import logger
 
 prometheus_client.REGISTRY.unregister(prometheus_client.GC_COLLECTOR)
 prometheus_client.REGISTRY.unregister(prometheus_client.PLATFORM_COLLECTOR)
@@ -13,29 +16,36 @@ app = FastAPI(debug=False)
 metrics_app = make_asgi_app()
 
 class BackgroundRunner:
-    def __init__(self, device, temperature_gauge, humidity_gauge, aqi_gauge):
-        self.device = device
-        self.temperature_gauge = temperature_gauge
-        self.humidity_gauge = humidity_gauge
-        self.aqi_gauge = aqi_gauge
+    def __init__(self, location, device_ip, device_token):
+        self.location = location
+        self.device_ip = device_ip
+        self.device_token = device_token
+        self.device = self.connect_device()
+        self.device_model = location + self.device.info().model
+        self.dev_metrics = re.sub('[^0-9a-zA-Z]+', '_', self.device_model).lower()
+        self.temperature_gauge = Gauge('{dev_metrics}_temperature'.format(dev_metrics=self.dev_metrics), 'Temperature of {dev_model}'.format(dev_model=self.device_model))
+        self.humidity_gauge = Gauge('{dev_metrics}_humidity'.format(dev_metrics=self.dev_metrics), 'Humidity of {dev_model}'.format(dev_model=self.device_model))
+        self.aqi_gauge = Gauge('{dev_metrics}_aqi'.format(dev_metrics=self.dev_metrics), 'Aqi of {dev_model}'.format(dev_model=self.device_model))
 
+    def connect_device(self):
+        return DeviceFactory.create(self.device_ip, self.device_token)
+    
     async def run_main(self):
         while True:
-            self.temperature_gauge.set(self.device.status().temperature)
-            self.humidity_gauge.set(self.device.status().humidity)
-            self.aqi_gauge.set(self.device.status().aqi)
+            try:
+                self.temperature_gauge.set(self.device.status().temperature)
+                self.humidity_gauge.set(self.device.status().humidity)
+                self.aqi_gauge.set(self.device.status().aqi)
+            except:
+                logger.error(traceback.format_exc())
+                self.device = self.connect_device()
             await asyncio.sleep(30)
+
 
 @app.on_event('startup')
 async def app_startup():
     try:
-        dev = DeviceFactory.create(os.environ.get('device_ip', ''), os.environ.get('device_token', ''))
-        dev_model = dev.info().model
-        tem = Gauge('miio_temperature_gauge', 'Temperature of {dev_model}'.format(dev_model=dev_model))
-        hum = Gauge('miio_humidity_gauge', 'Humidity of {dev_model}'.format(dev_model=dev_model))
-        aqi = Gauge('miio_aqi_gauge', 'Aqi of {dev_model}'.format(dev_model=dev_model))
-
-        runner = BackgroundRunner(dev, tem, hum, aqi)
+        runner = BackgroundRunner('', os.environ.get('device_ip', ''), os.environ.get('device_token', ''))
         asyncio.create_task(runner.run_main())
     except:
         exit()
